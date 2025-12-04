b4fa750604f882db68301c7d9e1348db0cb8c2c9
bhavesh-sagar <bhaveshnt9@gmail.com>
2025-12-04 19:27:27 +0530

    aboutme

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,7 +9,7 @@ export class ProjectsComponent {
     projects = 
     [
         { title: 'Food For All', desc: 'Platform to share leftover food safely.', img: 'assets/images/pic.png' },
-        { title: 'Chat Bot', desc: 'User Interaction with Simple chat bot.', img: 'assets/images/project2.jpg' },
+        { title: 'Chat Bot', desc: 'User Interaction with AI Chatbot with RAG.', img: 'assets/images/project2.jpg' },
         { title: 'Student Faculty Interaction', desc: 'Interaction between faculties and students.', img: 'assets/images/project3.png' }
     ];
 }
